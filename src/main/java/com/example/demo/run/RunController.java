7236f93de2d847756193cda7e8e8a957bd1a2de0
Nijar <nijarr2020@gmail.com>
2024-08-03 22:53:48 +0530

    other methods

--- src/main/java/com/example/demo/run/RunController.java
+++ src/main/java/com/example/demo/run/RunController.java
@@ -4,20 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import com.example.demo.DemoApplication;
 
 /**
  * RunController
  */
 
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
     private final RunRepository runRepository;
 
@@ -25,22 +36,36 @@ public class RunController {
         this.runRepository = runRepository;
     }
 
-    @GetMapping("/api/runs")
+    @GetMapping("")
     List<Run> findAll() {
         return runRepository.findAll();
     }
 
-    @GetMapping("/api/runs/{id}")
+    @GetMapping("/{id}")
     Run findById(@PathVariable Integer id) {
         Optional<Run> run = runRepository.findById(id);
         if (run.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND); 
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         return run.get();
     }
 
-    @PutMapping("/api/runs")
-    List<Run> updateAll() {
-        return runRepository.findAll();
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    void create(@RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("{id}")
+    void updateById(@RequestBody Run run, @PathVariable Integer id) {
+        log.info("data :" + run);
+        runRepository.updateById(run, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    void deleteById(@PathVariable Integer id) {
+        runRepository.deleteById(id);
     }
 }
--- src/main/java/com/example/demo/run/RunRepository.java
+++ src/main/java/com/example/demo/run/RunRepository.java
@@ -6,13 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+
+import com.example.demo.DemoApplication;
 
 import jakarta.annotation.PostConstruct;
 
 @Repository
 public class RunRepository {
-
+  private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
     private List<Run> runs = new ArrayList<>();
 
     List<Run> findAll() {
@@ -27,6 +31,22 @@ public class RunRepository {
         return runs;
     }
 
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void updateById(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        log.info("update "+ existingRun);
+        if (existingRun.isPresent()) {
+            runs.set(runs.indexOf(existingRun.get()), run);
+        }
+    }
+
+    void deleteById(Integer id) {
+        runs.removeIf(run -> run.id().equals(id));
+    }
+    
     @PostConstruct
     private void init() {
         runs.add(new Run("Monday", 1, LocalDateTime.now(), LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 4,
